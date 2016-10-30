7211e9784b0424618f2f551b37eab47975ec4226
jsoni2 <jakzi7005@gmail.com>
2016-10-30 13:50:17 -0700

    Project and Task Controller modified

--- src/main/java/spiderhub/web/controller/TaskController.java
+++ src/main/java/spiderhub/web/controller/TaskController.java
@@ -26,56 +26,74 @@ public class TaskController {
 
 	@Autowired
 	private UserDao userDao;
-
-	@RequestMapping("/projects/task/list.html")
-	public String list(ModelMap models) {
+	
+	@RequestMapping("/manager/projects/listTasks.html")
+	public String managerlist(ModelMap models) {
 		// get all users from databases and pass them to JSP
 
 		List<Task> tasks = taskDao.getTasks();
 		models.put("tasks", tasks);
-		return "projects/task/list";
+		return "manager/projects/listTasks";
 	}
+	
+	@RequestMapping("/member/projects/listTasks.html")
+	public String memberlist(ModelMap models) {
+		// get all users from databases and pass them to JSP
 
-	@RequestMapping("/projects/task/view.html")
+		List<Task> tasks = taskDao.getTasks();
+		models.put("tasks", tasks);
+		return "member/projects/listTasks";
+	}
+	
+	@RequestMapping("/manager/projects/viewTask.html")
+	// optional required = false
+	public String managerview(@RequestParam(required = false) Integer id, ModelMap models) {
+		// get user from database and pass it to JSP
+		models.put("task", taskDao.getTask(id));
+		return "manager/projects/viewTask";
+
+	}
+	
+	@RequestMapping("/member/projects/viewTask.html")
 	// optional required = false
 	public String view(@RequestParam(required = false) Integer id, ModelMap models) {
 		// get user from database and pass it to JSP
 		models.put("task", taskDao.getTask(id));
-		return "projects/task/view";
+		return "member/projects/viewTask";
 
 	}
-
-	@RequestMapping(value = "/projects/task/add.html", method = RequestMethod.GET)
+	
+	@RequestMapping(value = "/manager/projects/addTask.html", method = RequestMethod.GET)
 	public String add(ModelMap models) {
 		models.put("task", new Task());// once this is done we no longer need to
 										// add individual parameter
-		return "projects/task/add";
+		return "manager/projects/addTask";
 	}
 
-	@RequestMapping(value = "/projects/task/add.html", method = RequestMethod.POST)
+	@RequestMapping(value = "/manager/projects/addTask.html", method = RequestMethod.POST)
 	public String add(@ModelAttribute Task task) {
 
 		// save user to database
 		task.setCreateDate(new Date());
 		task = taskDao.saveTask(task);
 		// redirect to user list
-		return "redirect:list.html";
+		return "redirect:listTasks.html";
 	}
 
-	@RequestMapping(value = "/projects/task/assign.html", method = RequestMethod.GET)
+	@RequestMapping(value = "/manager/projects/assignTask.html", method = RequestMethod.GET)
 	public String assign(@RequestParam Integer id, ModelMap models) {
 		models.put("task", new Task());
 		models.put("users", userDao.getUsertoAddInProject());
-		return "projects/task/assign";
+		return "manager/projects/assignTask";
 	}
 
-	@RequestMapping(value = "/projects/task/assign.html", method = RequestMethod.POST)
+	@RequestMapping(value = "/manager/projects/assignTask.html", method = RequestMethod.POST)
 	public String assign(@RequestParam Integer id, @ModelAttribute Task task) {
 
 		// save user to database
 		task = taskDao.saveTask(task);
 		// redirect to user list
-		return "redirect:list.html";
+		return "redirect:listTasks.html";
 	}
 
 }
