938338ae45f6acc18ac343dd1eed1ca9855aa777
takayush <ayush.tak@gmail.com>
2016-11-08 23:32:14 -0800

    check if email already exist

--- src/main/java/spiderhub/web/validator/UserValidator.java
+++ src/main/java/spiderhub/web/validator/UserValidator.java
@@ -3,12 +3,14 @@ package spiderhub.web.validator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 import spiderhub.model.User;
+import spiderhub.model.dao.UserDao;
 
 @Component
 public class UserValidator implements Validator {
@@ -18,6 +20,10 @@ public class UserValidator implements Validator {
 	private Pattern pattern;
 	private Matcher matcher;
 
+	@Autowired
+	private UserDao userDao;
+	
+	//helper method
 	private boolean validateEmail(String email) {
 		pattern = Pattern.compile(EMAIL_PATTERN);
 
@@ -33,17 +39,24 @@ public class UserValidator implements Validator {
 	@Override
 	public void validate(Object target, Errors errors) {
 		User user = (User) target;
+		String email = user.getEmailAddress();
 		if (!StringUtils.hasText(user.getUserName()))
 			errors.rejectValue("userName", "error.field.empty");
 
 		if (!StringUtils.hasText(user.getPassword()))
 			errors.rejectValue("password", "error.field.empty");
 
-		if (!StringUtils.hasText(user.getEmailAddress()))
+		if (!StringUtils.hasText(email))
 			errors.rejectValue("emailAddress", "error.field.empty");
-		else if (!validateEmail(user.getEmailAddress())) {
+		else if (!validateEmail(email)) {
 			// check using regex whether it is
 			errors.rejectValue("emailAddress", "error.field.email");
+		}
+		else if(StringUtils.hasText( email)){
+			User u = userDao.checkEmailExist(email);
+			if(u != null ){
+				errors.rejectValue("emailAddress", "error.field.emailExist");
+			}
 		}
 
 		if (!StringUtils.hasText(user.getPhoneNumber()))
